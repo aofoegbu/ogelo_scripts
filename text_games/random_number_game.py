9e6a25905f7667ae50b1c3c9b87b1dbb059fc497
Augustine Ofoegbu <augustineogelo1@gmail.com>
2026-06-22 20:55:10 -0700

    New update

--- text_games/random_number_game.py
+++ text_games/random_number_game.py
@@ -3,7 +3,7 @@ import sys
 
 secret_number = random.randint(1, 20)
 print('I am thinking of a number between 1 and 20')
- 
+   
 # Ask the user to guess 6 times
 print("You have 6 guesses to find the right number")
 for guesses_taken in range(1, 7):
